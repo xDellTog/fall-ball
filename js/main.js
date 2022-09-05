3d70b82badf29bafd1cbd032053f17ba9301cc4e
Daniel Tognon <37959139+xDellTog@users.noreply.github.com>
2022-09-05 13:13:27 -0300

    test | matterjs

--- js/main.js
+++ js/main.js
@@ -1,20 +1,102 @@
-import Ball from "./Ball.js";
-import Canvas from "./Canvas.js";
-import {start} from "./utils.js";
+const RADIUS = 10;
 
-const canvas = new Canvas(document.getElementById('canvas'));
-const ctx = canvas.el.getContext('2d');
+function start() {
+    let {
+        Engine,
+        Render,
+        Runner,
+        Bodies,
+        Composite
+    } = Matter;
 
-let balls = [];
+    let engine = Engine.create();
 
-addEventListener('click', (ev) => {
-    balls.push(new Ball(ctx, ev.x, ev.y, 0, 0));
-});
-
-start((delta) => {
-    ctx.clearRect(0, 0, canvas.el.width, canvas.el.height);
-
-    balls.forEach((ball) => {
-        ball.update(delta);
+    let render = Render.create({
+        element: document.body,
+        engine: engine,
+        options: {
+            height: window.innerHeight,
+            width: window.innerWidth,
+        }
     });
-});
+
+    window.addEventListener('resize', () => {
+        render.canvas.height = window.innerHeight;
+        render.canvas.width = window.innerWidth;
+    });
+
+    window.addEventListener('mousemove', (ev) => {
+        console.log({
+            x: ev.clientX,
+            y: ev.clientY,
+        });
+    });
+
+    let dots = [];
+    let rows = 12;
+    let cols = 8;
+
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            if (j % 2 !== 0) {
+                if ((i + 1) === rows) continue;
+                dots.push(Bodies.circle((window.innerWidth / (rows + 1) * (i + 1.5)), (window.innerHeight / (rows + 1) * (j + 1)), RADIUS, {
+                    isStatic: true,
+                    restitution: 1,
+                    friction: 0,
+                }));
+            } else {
+                dots.push(Bodies.circle((window.innerWidth / (rows + 1) * (i + 1)), (window.innerHeight / (rows + 1) * (j + 1)), RADIUS, {
+                    isStatic: true,
+                    restitution: 1,
+                    friction: 0,
+                }));
+            }
+        }
+    }
+    let ground = Bodies.rectangle(window.innerWidth / 2, window.innerHeight - (RADIUS / 2), window.innerWidth, RADIUS, {
+        isStatic: true
+    });
+    let wallL = Bodies.rectangle((RADIUS / 2), window.innerHeight / 2, RADIUS, window.innerHeight, {
+        isStatic: true
+    });
+    let wallR = Bodies.rectangle(window.innerWidth - (RADIUS / 2), window.innerHeight / 2, RADIUS, window.innerHeight, {
+        isStatic: true,
+    });
+
+    Composite.add(engine.world, [
+        // player,
+        ...dots,
+        wallL,
+        wallR,
+        ground,
+    ]);
+
+    Render.run(render);
+
+    let runner = Runner.create();
+
+    Runner.run(runner, engine);
+
+    window.addEventListener('mouseup', (ev) => {
+        console.log({
+            x: ev.clientX,
+            y: ev.clientY,
+        });
+        let x = ev.clientX;
+        let y = ev.clientY;
+        let player = Bodies.circle(x, y, RADIUS, {
+            restitution: 1,
+            friction: 0,
+            render: {
+                fillStyle: '#FFF',
+            },
+        });
+        Composite.add(engine.world, [
+            player,
+        ]);
+    });
+}
+
+
+start();
